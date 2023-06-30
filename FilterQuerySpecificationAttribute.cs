c3c12f7a226d1e1054d840934fb38744808d7c70
vijay@envivo <vijay.patel@envivo.co.uk>
2023-06-30 09:27:02 +0100

    feat: Added FilterQuerySpecificationAttribute.RunWhenContextChanges

--- FilterQuerySpecificationAttribute.cs
+++ FilterQuerySpecificationAttribute.cs
@@ -14,14 +14,20 @@ namespace Envivo.Fresnel.ModelAttributes
         /// <inheritdoc/>
         /// </summary>
         /// <param name="specificationType"></param>
-        public FilterQuerySpecificationAttribute(Type specificationType = null)
+        public FilterQuerySpecificationAttribute(Type specificationType = null, bool runWhenContextChanges = false)
         {
             this.SpecificationType = specificationType;
+            this.RunWhenContextChanges = runWhenContextChanges;
         }
 
         /// <summary>
         /// The type of the <c>IQuerySpecification</c> used for this member 
         /// </summary>
         public Type SpecificationType { get; }
+
+        /// <summary>
+        /// Determines if the <c>IQuerySpecification</c> should be run whenever the parent object's values change
+        /// </summary>
+        public bool RunWhenContextChanges { get; }
     }
 }
