dca4df4d2c558a0b99600b047adfa82d75fafaa1
vijay@envivo <vijay.patel@envivo.co.uk>
2023-10-08 17:52:38 +0100

    refact(FilterQuerySpecification): Removed obsolete RunWhenContextChanges

--- FilterQuerySpecificationAttribute.cs
+++ FilterQuerySpecificationAttribute.cs
@@ -14,20 +14,15 @@ namespace Envivo.Fresnel.ModelAttributes
         /// <inheritdoc/>
         /// </summary>
         /// <param name="specificationType"></param>
-        public FilterQuerySpecificationAttribute(Type specificationType = null, bool runWhenContextChanges = false)
+        /// <param name="runWhenContextChanges"></param>
+        public FilterQuerySpecificationAttribute(Type specificationType = null)
         {
             this.SpecificationType = specificationType;
-            this.RunWhenContextChanges = runWhenContextChanges;
         }
 
         /// <summary>
         /// The type of the <c>IQuerySpecification</c> used for this member 
         /// </summary>
         public Type SpecificationType { get; }
-
-        /// <summary>
-        /// Determines if the <c>IQuerySpecification</c> should be run whenever the parent object's values change
-        /// </summary>
-        public bool RunWhenContextChanges { get; }
     }
 }
