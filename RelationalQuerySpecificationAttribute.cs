b318d5a05d623f8e8abe9f749fb4fed6f96dcff6
vijay@envivo <vijay.patel@envivo.co.uk>
2023-11-27 16:43:49 +0000

    feat: Added RelationalQuerySpecificationAttribute

---
+++ RelationalQuerySpecificationAttribute.cs
@@ -0,0 +1,28 @@
+// SPDX-FileCopyrightText: Copyright (c) 2022-2023 Envivo Software
+// SPDX-License-Identifier: LicenseRef-proprietary=www.envivo.co.uk/fresnel-eula
+using System;
+
+namespace Envivo.Fresnel.ModelAttributes
+{
+    /// <summary>
+    /// Declares the <c>IQuerySpecification</c> used to load contents for the associated member.
+    /// Note that this makes the collection immutable.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Parameter)]
+    public class RelationalQuerySpecificationAttribute : Attribute
+    {
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        /// <param name="specificationType"></param>
+        public RelationalQuerySpecificationAttribute(Type specificationType = null)
+        {
+            this.SpecificationType = specificationType;
+        }
+
+        /// <summary>
+        /// The type of the <c>IQuerySpecification</c> used for this member 
+        /// </summary>
+        public Type SpecificationType { get; }
+    }
+}
