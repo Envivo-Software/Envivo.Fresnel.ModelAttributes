a1b1ec10af50a8b25470a9c10f067f3a4c187db8
vijay@envivo <vijay.patel@envivo.co.uk>
2025-04-16 15:16:34 +0100

    feat: Added UiExplorerAttribute, to control default Explorer size

---
+++ UiExplorerAttribute.cs
@@ -0,0 +1,28 @@
+﻿// SPDX-FileCopyrightText: Copyright (c) 2022-2025 Envivo Software
+// SPDX-License-Identifier: LicenseRef-proprietary=www.envivo.co.uk/fresnel-eula
+using System;
+
+namespace Envivo.Fresnel.ModelAttributes
+{
+    /// <summary>
+    /// Declares the UI behaviour for the associated Class or Interface
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface)]
+    public class UiExplorerAttribute : Attribute
+    {
+        /// <inheritdoc/>
+        /// <param name="preferredSize"><inheritdoc cref="PreferredSize" path="/summary"/></param>
+        public UiExplorerAttribute
+        (
+            UiExplorerSize preferredSize = UiExplorerSize.Default
+        )
+        {
+            PreferredSize = preferredSize;
+        }
+
+        /// <summary>
+        /// The preferred starting size for the Class
+        /// </summary>
+        public UiExplorerSize PreferredSize { get; }
+    }
+}
---
+++ UiExplorerSize.cs
@@ -0,0 +1,30 @@
+﻿// SPDX-FileCopyrightText: Copyright (c) 2022-2025 Envivo Software
+// SPDX-License-Identifier: LicenseRef-proprietary=www.envivo.co.uk/fresnel-eula
+namespace Envivo.Fresnel.ModelAttributes
+{
+    /// <summary>
+    /// The sizes of Explorer cards 
+    /// </summary>
+    public enum UiExplorerSize
+    {
+        /// <summary>
+        /// The Explorer opens with its default size
+        /// </summary>
+        Default,
+
+        /// <summary>
+        /// The Explorer opens in its smallest form
+        /// </summary>
+        Minimised,
+
+        /// <summary>
+        /// Used when there are few contents in the Explorer
+        /// </summary>
+        Compact,
+
+        /// <summary>
+        /// Used when there are wide collection contents in the Explorer
+        /// </summary>
+        Maximised
+    }
+}
